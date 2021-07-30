d8a1f3305a87666162f5fb2e3ed47f1a6ad9d6a4
dpuentes <dipuentes123@gmail.com>
2021-07-30 00:09:44 -0500

    DB migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_023955) do
+ActiveRecord::Schema.define(version: 2021_07_30_050731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calories", force: :cascade do |t|
+    t.datetime "date_registre"
+    t.string "Calory_type"
+    t.float "Value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "commentaries", force: :cascade do |t|
+    t.text "Content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
