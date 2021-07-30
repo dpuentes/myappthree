fa7410f89352cc0d39f516d9a392f43fe9514924
dpuentes <dipuentes123@gmail.com>
2021-07-30 09:47:04 -0500

    Db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_023955) do
+ActiveRecord::Schema.define(version: 2021_07_30_144516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calories", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "date_registre"
+    t.string "Calory_type"
+    t.float "Value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_calories_on_user_id"
+  end
+
+  create_table "commentaries", force: :cascade do |t|
+    t.bigint "calory_id", null: false
+    t.text "Content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["calory_id"], name: "index_commentaries_on_calory_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +45,6 @@ ActiveRecord::Schema.define(version: 2021_07_29_023955) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "calories", "users"
+  add_foreign_key "commentaries", "calories"
 end
